4dc837ef0f70b2d06bdf1775b73186899a3e7b6f
malayvasa <malayvasa@gmail.com>
2021-07-05 23:15:21 +0530

    Added Framer Motion

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Link from '@/components/Link'
 import { PageSeo } from '@/components/SEO'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
+import SectionContainer from '@/components/SectionContainer'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
 import { motion } from 'framer-motion'
 
@@ -14,102 +15,111 @@ export async function getStaticProps() {
   return { props: { posts } }
 }
 
+function truncate(str) {
+  return str.length > 180 ? str.substring(0, 179) + '...' : str
+}
+
 export default function Home({ posts }) {
   return (
-    <motion.div
-      initial={{ opacity: 0, y: -10 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ ease: 'easeIn', duration: 1, type: 'spring' }}
-    >
-      <PageSeo
-        title={siteMetadata.title}
-        description={siteMetadata.description}
-        url={siteMetadata.siteUrl}
-      />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="pt-6 pb-8">
-          <h1 className="text-2xl md:text-5xl mb-4 font-extrabold tracking-tight text-gray-900 dark:text-gray-100">
-            Hey, I'm Malay Vasa{' '}
-            <span role="img" aria-label="emoji">
-              👋
-            </span>
-          </h1>
-          <p className="text-lg max-w-3xl text-gray-500 dark:text-gray-400">
-            I'm a designer, coder and creator. I currently work as a Product Design Intern at
-            <Link
-              href={'https://modfy.video/'}
-              className="ml-1 text-indigo-500 dark:text-indigo-500"
-            >
-              Modfy.
-            </Link>{' '}
-            Welcome to my corner of the Internet.
-          </p>
-        </div>
-        <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length && 'No posts found.'}
-          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
-            return (
-              <li key={slug} className="py-12">
-                <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>
-                          {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                        </time>
-                      </dd>
-                    </dl>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link
-                              href={`/blog/${slug}`}
-                              className="text-gray-900 dark:text-gray-100"
-                            >
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
+    <SectionContainer>
+      <motion.div
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ ease: 'easeIn', duration: 1, type: 'spring' }}
+      >
+        <PageSeo
+          title={siteMetadata.title}
+          description={siteMetadata.description}
+          url={siteMetadata.siteUrl}
+        />
+        <div className="divide-y divide-gray-200 dark:divide-gray-700">
+          <div className="pt-6 pb-8">
+            <h1 className="text-2xl md:text-5xl mb-4 font-extrabold tracking-tight text-gray-900 dark:text-gray-100">
+              Hey, I'm Malay Vasa{' '}
+              <span role="img" aria-label="emoji">
+                👋
+              </span>
+            </h1>
+            <p className="text-lg max-w-3xl text-gray-500 dark:text-gray-400">
+              I'm a designer, coder and creator. I currently work as a Product Design Intern at
+              <Link
+                href={'https://modfy.video/'}
+                className="ml-1 text-indigo-500 dark:text-indigo-500"
+              >
+                Modfy.
+              </Link>{' '}
+              Welcome to my corner of the Internet.
+            </p>
+          </div>
+          <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+            {!posts.length && 'No posts found.'}
+            {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+              const { slug, date, title, summary, tags } = frontMatter
+              return (
+                <li key={slug} className="py-12">
+                  <article>
+                    <div className="space-y-2 xl:grid xl:grid-cols-4 xl:space-y-0 xl:items-baseline">
+                      <dl>
+                        <dt className="sr-only">Published on</dt>
+                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                          <time dateTime={date}>
+                            {new Date(date).toLocaleDateString(
+                              siteMetadata.locale,
+                              postDateTemplate
+                            )}
+                          </time>
+                        </dd>
+                      </dl>
+                      <div className="space-y-5 xl:col-span-3">
+                        <div className="space-y-6">
+                          <div>
+                            <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                              <Link
+                                href={`/blog/${slug}`}
+                                className="text-gray-900 dark:text-gray-100"
+                              >
+                                {title}
+                              </Link>
+                            </h2>
+                            <div className="flex flex-wrap">
+                              {tags.map((tag) => (
+                                <Tag key={tag} text={tag} />
+                              ))}
+                            </div>
+                          </div>
+                          <div className="prose text-gray-500 max-w-none dark:text-gray-400">
+                            {truncate(summary)}
                           </div>
                         </div>
-                        <div className="prose text-gray-500 max-w-none dark:text-gray-400">
-                          {summary}
+                        <div className="text-base font-medium leading-6">
+                          <Link
+                            href={`/blog/${slug}`}
+                            className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
+                            aria-label={`Read "${title}"`}
+                          >
+                            Read more &rarr;
+                          </Link>
                         </div>
                       </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
-                          aria-label={`Read "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
                     </div>
-                  </div>
-                </article>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-      {posts.length > MAX_DISPLAY && (
-        <div className="flex justify-end text-base font-medium leading-6">
-          <Link
-            href="/blog"
-            className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
-            aria-label="all posts"
-          >
-            All Posts &rarr;
-          </Link>
+                  </article>
+                </li>
+              )
+            })}
+          </ul>
         </div>
-      )}
-    </motion.div>
+        {posts.length > MAX_DISPLAY && (
+          <div className="flex justify-end text-base font-medium leading-6">
+            <Link
+              href="/blog"
+              className="text-blue-500 hover:text-blue-600 dark:hover:text-blue-400"
+              aria-label="all posts"
+            >
+              All Posts &rarr;
+            </Link>
+          </div>
+        )}
+      </motion.div>
+    </SectionContainer>
   )
 }
